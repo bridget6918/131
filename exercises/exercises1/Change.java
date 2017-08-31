c75456efeaad62f0a56c9af597b2b9147b310215
Bridg <Bridg@Kingdom-69.home>
2017-08-30 21:27:03 -0500

    Exercise 1 Pennies break down

--- exercises/exercises1/Change.java
+++ exercises/exercises1/Change.java
@@ -9,6 +9,9 @@ public class Change {
 		// Below, prompt the user to enter a number of pennies
 		//
 		
+		ArgsProcessor ap = new ArgsProcessor(args);
+		
+		int initialPennies = ap.nextInt("Number of Pennies?");
 		
 		//
 		// Then, compute and print out how many 
@@ -16,6 +19,23 @@ public class Change {
 		// should be given in exchange for those pennies, so as to
 		// minimize the number of coins (see the videos)
 		//
+				
+		int dollars = initialPennies/100;
+		int penniesAfterDollars = initialPennies%100;
+		int quarters = penniesAfterDollars/25;
+		int penniesAfterQuarters = penniesAfterDollars%25;
+		int dimes = penniesAfterQuarters/10;
+		int penniesAfterDimes = penniesAfterQuarters%10;
+		int nickels = penniesAfterDimes/5;
+		int penniesAfterNickels = penniesAfterDimes%5;
+		int penniesLeft = initialPennies - (dollars*100 + quarters*25 + dimes*10 + nickels*5);
+		
+		System.out.println("For X amount of pennies: ");
+		System.out.println(dollars + " dollars");
+		System.out.println(quarters + " quarters");
+		System.out.println(dimes + " dimes");
+		System.out.println(nickels + " nickels");
+		System.out.println(penniesLeft + " pennies");
 	}
 
 }
