5370ac7a9c9eec7c4ff5eb13511412d3147e5baa
Bridg <Bridg@Kingdom-69.dhcp.wustl.edu>
2017-08-31 12:11:33 -0500

    rhs is computed first

---
+++ studios/studio1/Average.java
@@ -0,0 +1,19 @@
+package studio1;
+
+import cse131.ArgsProcessor;
+
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArgsProcessor ap = new ArgsProcessor(args);
+		int n1 = ap.nextInt("first number to be averaged?");
+		int n2 = ap.nextInt("second number to be averaged?");
+		
+		double avg = (double) (n1 + n2)/2;
+		
+		System.out.println(avg);
+
+	}
+
+}
