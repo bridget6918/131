0db6c0cea0d00d1b0ab72e3b7a5e15a18470afdb
Bridg <Bridg@Kingdom-69.dhcp.wustl.edu>
2017-09-05 14:07:48 -0500

    feet to inches exercise complete

--- exercises/exercises2/FeetInches.java
+++ exercises/exercises2/FeetInches.java
@@ -1,11 +1,15 @@
 package exercises2;
 
+import cse131.ArgsProcessor;
+
 public class FeetInches {
 
 	public static void main(String[] args) {
 		//
 		// Prompt the user for a number of inches
 		//
+		ArgsProcessor ap = new ArgsProcessor(args);
+		int inches = ap.nextInt("Enter inches:");
 		// Convert that into feet and inches
 		//   BUT
 		// Be sure to use the singular "foot" or "inch"
@@ -16,6 +20,32 @@ public class FeetInches {
 		//    the output
 		//   5 feet and 1 inch
 		//
+		int feet = inches / 12;
+		int inchesLeft = inches % 12;
+		// if statement to determmine whether singular or plural
+		if (feet == 1) {
+			if (inchesLeft == 1) {
+				System.out.println(inches + " inches will produce: "
+						+ feet + " foot and "
+						+ inchesLeft + " inch");
+			}
+			else {
+				System.out.println(inches + " inches will produce: "
+						+ feet + " foot and "
+						+ inchesLeft + " inches");
+			}
+		}
+		else {
+			if (inchesLeft == 1) {
+				System.out.println(inches + " inches will produce: "
+						+ feet + " feet and "
+						+ inchesLeft + " inch");
+			}
+			else {System.out.println(inches + " inches will produce: "
+						+ feet + " feet and "
+						+ inchesLeft + " inches");
+			}
+		}
 
 	}
 
