33753976631b0fda8ec6246a2af851817539f535
Bridg <Bridg@192.168.1.6>
2017-09-06 17:47:52 -0500

    random averages complete

--- exercises/exercises2/RandomAverages.java
+++ exercises/exercises2/RandomAverages.java
@@ -11,16 +11,25 @@ public class RandomAverages {
 		//     thus far.
 		//
 		// Your code goes below here.
-		
-		
+		int i = 0;
+		double number = 0;
+		while (i < 1000) {
+			double avg = number / i;
+			i = i + 1;
+			number = number + Math.random();
+			if (i % 100 == 0) {
+				System.out.println("As of " + i + " , the average is " + avg);
+			}
+		}
 		
 		//
 		//  Some questions:
 		//    1) How does the average change as your loop progresses?
 		//    2) Can you write code in your loop that prints out the average only
 		//         every 100 iterations, instead of each iteration?
-		//
-		
+		//	Answers: 
+		//		1) The average approaches 0.5. 
+
 	}
 
 }
