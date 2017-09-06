21b9572d18b2c9f1d770cc5077220291851dffd8
Bridg <Bridg@192.168.1.6>
2017-09-06 17:59:47 -0500

    print vs println

--- exercises/exercises2/Loops.java
+++ exercises/exercises2/Loops.java
@@ -9,11 +9,11 @@ public class Loops {
 		// will print the value of i but not skip to a new line
 		// in the output
 		//
-		//    System.out.println(i + "  ");
+		//    System.out.print(i + "  ");
 		// will print the value of i with a few spaces after it, also
 		// not skipping to a new line
 		//
-		//    System.out.println();
+		//    System.out.print();
 		// skips to the next line
 		//
 		//
@@ -43,18 +43,39 @@ public class Loops {
 		
 		// 0 to 9 inclusively
 		//    produces  0 1 2 3 4 5 6 7 8 9
-		
+		int i1 = 0;
+		while (i1 <= 9) {
+			System.out.print(i1 + " ");
+			i1 = i1 + 1;
+		}
+		System.out.println();
 		
 		// 1 to 10 inclusively
 		//    produces 1 2 3 4 5 6 7 8 9 10
-		
+		int i2 = 1;
+		while (i2 <= 10) {
+			System.out.print(i2 + " ");
+			i2 = i2 + 1;
+		}
+		System.out.println();
 		
 		// 0 to 10, including 0, excluding 10
 		//    produces 0 1 2 3 4 5 6 7 8 9
-		
+		int i3 = 0;
+		while (i3 < 10) {
+			System.out.print(i3 + " ");
+			i3 = i3 + 1;
+		}
+		System.out.println();
 		
 		// like the one above, but jumping by 2
 		//    produces 0 2 4 6 8
+		int i4 = 0;
+		while (i4 < 10) {
+			System.out.print(i4 + " ");
+			i4 = i4 + 2;
+		}
+		System.out.println();
 	}
 
 }
