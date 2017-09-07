c86491cf03cba8dc756576aa5cee70672f230e5a
Bridg <Bridg@192.168.1.6>
2017-09-06 20:21:08 -0500

    100 runs complete. i++ vs ++i?

--- exercises/exercises2/Heads.java
+++ exercises/exercises2/Heads.java
@@ -6,23 +6,28 @@ public class Heads {
 
 	public static void main(String[] args) {
 
-		// what are the concepts?
-		int numHeads = 0;
-		int numFlips = 0;
+		//beginning of the for loop
+		for (int number = 0; number < 100; number = number + 1) {
 
-		// now flip a coin until we see 10 heads
-		while (numHeads != 10) {
-			boolean isHeads = Math.random() < 0.5;
-			if (isHeads) {
-				numHeads = numHeads + 1;
+			// what are the concepts?
+			int numHeads = 0;
+			int numFlips = 0;
+
+			// now flip a coin until we see 10 heads
+			while (numHeads != 10) {
+				boolean isHeads = Math.random() < 0.5;
+				if (isHeads) {
+					numHeads = numHeads + 1;
+				}
+				numFlips = numFlips + 1;
+
 			}
-			numFlips = numFlips + 1;
+			// here, numHeads should be 10
 
+			System.out.println("Number of flips was " + numFlips);
+
+			// end of the for loop
 		}
-		// here, numHeads should be 10
-		
-		System.out.println("Number of flips was " + numFlips);
-
 	}
 
 }
