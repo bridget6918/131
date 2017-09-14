67c1c88935c976500018521afec600570cb04fa4
Bridg <Bridg@192.168.1.6>
2017-09-13 22:17:23 -0500

    shuffle array exercise complete

--- exercises/exercises3/Shuffle.java
+++ exercises/exercises3/Shuffle.java
@@ -6,11 +6,36 @@ public class Shuffle {
 		String[] original = { "A", "B", "C", "D",
 				"E", "F", "G", "H"
 		};
+		
+		// copy the original array
+		String[] shuffled = new String[original.length];
+		for (int i = 0; i < original.length; ++i) {
+			shuffled [i] = original [i];
+		}
+		// to iterate backwards
+		for (int i = shuffled.length - 1; i >= 0; --i) {
+			// this will generate an integer from 0 to i
+			int c = (int) (Math.random() * (i + 1)); 
+			// once the number/card is generated, 
+			// we want to pick the card and set it to the array
+			shuffled[i] = original[c];
+			
+			// a loop that moves all the cards up from c+1 to length-1
+			for (int j = c; j < shuffled.length - 1; ++j) {
+				original[j] = original[j+1];
+			}
+		}
 
 		// print out original array
 		for (int i=0; i < original.length; ++i) {
 			System.out.println("Original at " + i + " is " + original[i]);
 		}
+		
+		// print out the shuffled array
+		for (int i = 0; i < shuffled.length; ++i) {
+			System.out.println("Shuffled at " + i + " is " + shuffled[i]);
+		}
+		
 
 		//
 		// Follow the instructions on the web page to make a copy of
