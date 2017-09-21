42b7a406717648bfa150cc1a25d520df2cbb139d
Bridg <Bridg@Kingdom-69.home>
2017-09-20 22:24:03 -0500

    exercise graphics complete

--- exercises/exercises4/GraphicsDemo.java
+++ exercises/exercises4/GraphicsDemo.java
@@ -8,15 +8,24 @@ public class GraphicsDemo {
 
 	public static void main(String[] args) {
 		// blue point (look carefully, will be very small on your screen)
-		
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.point(0.3, 0.3);
 		// larger green point
-		
+		StdDraw.setPenColor(Color.GREEN);
+		StdDraw.setPenRadius(.05);
+		StdDraw.point(0.7, 0.15);
 		// unfilled red triangle
-		
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.setPenRadius(0.02);
+		StdDraw.line(0, 0, 0, 0.5);
+		StdDraw.line(0, 0.5, 0.25, 0);
+		StdDraw.line(0.25, 0, 0, 0);
 		// yellow circle, filled
-
+		StdDraw.setPenColor(Color.YELLOW);
+		StdDraw.filledCircle(0.3, 0.7, 0.08);
 		// filled blue rectangle
-		
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledRectangle(0.7, 0.4, 0.02, 0.04);
 
 
 	}
