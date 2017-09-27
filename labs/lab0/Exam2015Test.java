b3e956d24b24f0f0e76097a4dd6d1c337a81dcdd
Bridg <Bridg@Kingdom-69.dhcp.wustl.edu>
2017-09-27 15:00:52 -0500

    practice exam code update

--- labs/lab0/Exam2015Test.java
+++ labs/lab0/Exam2015Test.java
@@ -20,20 +20,21 @@ public class Exam2015Test {
 		//		}
 		//		System.out.println(min);
 
-		// Question 3
-		//		int N = ap.nextInt();
-		//		double [] nums = new double[N];
-		//		for (int i=0; i < N; ++i) {
-		//			double r = 1.5 * Math.random();
-		//			nums[i] = r;
-		//		}
-		//		int count = 0;
-		//		for (int i=0; i < N; ++i) {
-		//			if (nums[i] < 0.5) {
-		//				count++;
-		//			}
-		//		}
-		//		System.out.println(count);
+		// Question 3 and Q3 from 2013 Exam combined
+		//				int N = ap.nextInt();
+		//				double [] nums = new double[N];
+		//				for (int i=0; i < N; ++i) {
+		//					double r = 1.5 * Math.random();
+		//					nums[i] = r;
+		//				}
+		//				int count = 0;
+		//				for (int i=0; i < N; ++i) {
+		//					if (nums[i] < 0.5) {
+		//						count++;
+		//					}
+		//				}
+		//				double percent = Math.round((count*1.0/N * 1000.0))/10.0; 
+		//				System.out.println(count + " " + percent + "%");
 
 		// Question 4
 		//		String [] ht = new String [20];
@@ -88,9 +89,9 @@ public class Exam2015Test {
 		//		System.out.println(d + "\n" + r1 + "\n" + r2 + "\n" + k + "\n" + s);
 
 		// Question #4
-		int N = 8; // number of people being polled
-		int [] poll = {3, 1, 3, 7, 6, 1, 3, 2};
-		int M = 7; // number of candidates running for the poll
+		//		int N = 8; // number of people being polled
+		//		int [] poll = {3, 1, 3, 7, 6, 1, 3, 2};
+		//		int M = 7; // number of candidates running for the poll
 
 		// Solution 1
 		//		for (int c=0; c < M; ++c) {
@@ -104,15 +105,68 @@ public class Exam2015Test {
 		//		}
 
 		// Solution 2
-		int [] count = new int [N];
-		int index = 0;
+		//		int [] count = new int [N];
+		//		int index = 0;
+		//
+		//		for (int c=0; c < N; ++c) {
+		//				index = poll[c];
+		//				count[index-1] = count[index-1] + 1;				
+		//		}
+		//		for (int i=0; i < M; ++i) {
+		//			System.out.println("Candidate " + (i+1) + " received " + count[i] + " votes");
+		//		}
 
-		for (int c=0; c < N; ++c) {
-				index = poll[c];
-				count[index-1] = count[index-1] + 1;				
+		// Exam Fall 2013
+
+		// Question 2 (a)
+		//		int a = ap.nextInt("a");
+		//		int b = ap.nextInt("b");
+		//		int c = ap.nextInt("c");
+		//		
+		//		if (a!=b && b!=c && c!= a) {
+		//			System.out.println(true);
+		//		}
+		//		else {
+		//			System.out.println(false);
+		//		}
+
+		// (b) 
+		//		int N = ap.nextInt("#?");
+		//		double sum = 0;
+		//		for (int i=0; i < N; ++i) {
+		//			sum = sum + Math.random();
+		//		}
+		//		double avg = sum / N;
+		//		System.out.println(avg);
+
+		// (c) 
+		//		int N = ap.nextInt("#?");
+		//		double max = 0;
+		//		for (int i=0; i < N; ++i) {
+		//			double r = Math.random();
+		//			if (r > max) {
+		//				max = r;
+		//			}
+		//		}
+		//		System.out.println(max);
+
+		// Question 4
+		int N = ap.nextInt("How many?");
+		boolean [] sieve = new boolean [N];
+
+		for (int i=0; i < N; ++i) {
+			sieve[i] = true;
 		}
-		for (int i=0; i < M; ++i) {
-			System.out.println("Candidate " + (i+1) + " received " + count[i] + " votes");
+
+		for (int i=1; i < N; ++i) {
+			for (int j=7*i; j < N; j = j+7) {
+				sieve[j] = false;
+				sieve[j+1] = false;
+				sieve[j-1] = false;
+			}
+			if (sieve[i]) {
+				System.out.print(i + " ");
+			}
 		}
 	}
 }
