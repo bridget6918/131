24d228172d36440380cc84f36e4ec885f00d5ef0
Bridg <Bridg@Kingdom-69.dhcp.wustl.edu>
2017-10-05 12:22:48 -0500

    studio 5 complete

--- studios/studio5/Methods.java
+++ studios/studio5/Methods.java
@@ -21,6 +21,67 @@ public class Methods {
 	public static int mpy(int x, int y) {
 		return 0;  // FIXME
 	}
-
+	
+	/** 
+	 * 
+	 * @param a one integer
+	 * @param b another integer
+	 * @param c the third integer
+	 * @return average of the three
+	 */
+	public static double avg3(int a, int b, int c) {
+		double average = (a + b + c) / 3.0;
+		return average;
+	}
+	
+	/**
+	 * 
+	 * @param array an array of doubles
+	 * @return sum of the array as a double
+	 */
+	public static double sumArray(double [] array) {
+		double sum = 0;
+		for (int i=0; i < array.length; ++i) {
+			sum = sum + array[i];
+		}
+		return sum;
+	}
+	
+	/**
+	 * 
+	 * @param array an array of doubles
+	 * @return average of the array as a double
+	 */
+	public static double average(double [] array) {
+		double average = 0; 
+		for (int i=0; i < array.length; ++i) {
+			average = sumArray(array) / array.length;
+		}
+		return average;
+	}
+	
+	/**
+	 * 
+	 * @param array an array of doubles
+	 * @return the max of the array as a double
+	 */
+	public static double max(double [] array) {
+		double max = Double.MIN_VALUE;
+		for (int i=0; i < array.length; ++i) {
+			if (max < array[i]) {
+				max = array[i];
+			}
+		}
+		return max;
+	}
+	
+	/**
+	 * 
+	 * @param s input a string
+	 * @return the string begins with the second letter + the first letter + ay
+	 */
+	public static String pig(String s) {
+		return s.substring(1) + s.substring(0, 1) + "ay";
+	}
 
 }
