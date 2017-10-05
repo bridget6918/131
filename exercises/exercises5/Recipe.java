9375e63193d0d972162ce18d99db76c6f73a152a
Bridg <Bridg@Kingdom-69.home>
2017-10-04 21:44:58 -0500

    exercise recipe complete

--- exercises/exercises5/Recipe.java
+++ exercises/exercises5/Recipe.java
@@ -9,12 +9,20 @@ public class Recipe {
 		// Below, use comments or actual Java code
 		//  to type in the abstractions
 		//  you found in the recipe story
-		// 
+		// Abstractions found: mixWithSpoon and heatUp
 
 		//
 		// Use those abstractions in comments or code to 
 		//   express the recipe
 		//
+		// heatUp(water, 100 degrees, noTime)
+		// mixWithSpoon(water, yeast, slow)
+		// mixWithSpoon(butter, vanilla, slow)
+		// after 5 minutes, mixWithSpoon(yeast, butter, sugar, flour)
+		// heatUp(mixture, 110 degrees, 45 minutes)
+		// divide the mixture into 10 equally sized portions
+		// heatUp(mixture, 350 degrees, 45 minutes)
+		
 	}
 
 }
