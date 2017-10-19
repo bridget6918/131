45eab1eb215b2698d3c04ca634d41e997f413103
Bridg <Bridg@Kingdom-69.home>
2017-10-19 01:25:41 -0500

    not sure what to do with this exercise so I just fixed it by adding the base case

--- exercises/exercises6/FactorialNoBaseCase.java
+++ exercises/exercises6/FactorialNoBaseCase.java
@@ -1,9 +1,14 @@
 package exercises6;
 
 public class FactorialNoBaseCase {
-	
+
 	public static int factorial(int n) {
-		return n * factorial(n-1);
+		if (n <= 0) {
+			return 1;
+		}
+		else {
+			return n * factorial(n-1);
+		}
 	}
 
 	public static void main(String[] args) {
