cd348c2fae095c5a3911fabb3ac2ff8e8702635a
Bridg <Bridg@Kingdom-69.dhcp.wustl.edu>
2017-10-19 12:26:57 -0500

    updated javadoc

--- studios/studio6/Methods.java
+++ studios/studio6/Methods.java
@@ -3,6 +3,11 @@ package studio6;
 public class Methods {
 
 	// Your methods go below this line
+	/**
+	 * 
+	 * @param n int n
+	 * @return factorial of n
+	 */
 	public static int fact(int n) {
 		if (n <= 0) {
 			return 1;
@@ -12,6 +17,11 @@ public class Methods {
 		}
 	}
 	
+	/**
+	 * 
+	 * @param n integer n
+	 * @return return Fibonacci series
+	 */
 	public static int fib(int n) {
 		if (n <= 1) {
 			return n;
@@ -21,6 +31,11 @@ public class Methods {
 		}
 	}
 	
+	/**
+	 * 
+	 * @param n integer n
+	 * @return whether n is an odd number
+	 */
 	public static boolean isOdd (int n) {
 		if (n <= 0) {
 			return false;
@@ -30,6 +45,12 @@ public class Methods {
 		}
 	}
 	
+	/**
+	 * 
+	 * @param a integer a
+	 * @param b integer b
+	 * @return sum of a and b
+	 */
 	public static int sum(int a, int b) {
 		if (b <= 0) {
 			return a;
@@ -39,6 +60,11 @@ public class Methods {
 		}
 	}
 	
+	/**
+	 * 
+	 * @param n integer n
+	 * @return sum of n + (n-2) + (n-2) + ... + 0
+	 */
 	public static int sumDownBy2(int n) {
 		if (n == 0) {
 			return 0;
@@ -51,6 +77,11 @@ public class Methods {
 		}
 	}
 	
+	/**
+	 * 
+	 * @param n integer n
+	 * @return harmonic sum of n (1/n + 1/(n-1) + ... + 1)
+	 */
 	public static double harmonicSum(double n) {
 		if (n == 1.0) {
 			return 1.0;
@@ -60,6 +91,12 @@ public class Methods {
 		}
 	}
 	
+	/**
+	 * 
+	 * @param a integer a
+	 * @param b integer b
+	 * @return a*b
+	 */
 	public static int mult(int a, int b) {
 		if (b == 0) {
 			return 0;
