63ed573a79147871b721139531e5fc192355d994
Bridg <Bridg@Kingdom-69.home>
2017-10-24 01:59:58 -0500

    lab 6 methods updated

--- labs/lab6/Methods.java
+++ labs/lab6/Methods.java
@@ -18,6 +18,7 @@ public class Methods {
 			return f(f(x + 11));
 		}
 	}
+	// for f(), if x > 100, it gives x - 10; if x <= 100, it returns 91
 	
 	/**
 	 * 
@@ -34,7 +35,8 @@ public class Methods {
 		}
 		else {
 			return g(x-1, g(x, y-1));
-			// when x = 4, y will be 0, so stackoverflow
+			// for x=4, y=0, it returns 13
+			// for x=4, y=other values, stack over flow error
 		}
 	}
 
@@ -47,7 +49,16 @@ public class Methods {
 		// from here, call f or g with the appropriate parameters
 		//
 		System.out.println(f(87));
+		System.out.println(f(91));
+		System.out.println(f(113));
+		System.out.println(f(1039));
+		System.out.println(f(43));
+		System.out.println();
+		
 		System.out.println(g(3,2));
+		System.out.println(g(1,100));
+		System.out.println(g(4,0));
+		System.out.println(g(2,0));
 	}
 
 }
