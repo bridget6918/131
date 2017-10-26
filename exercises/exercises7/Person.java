4e53efad582d796565de85bf172c2a0825cfa951
Bridg <Bridg@Kingdom-69.home>
2017-10-26 09:47:36 -0500

    exercise 7 complete

--- exercises/exercises7/Person.java
+++ exercises/exercises7/Person.java
@@ -13,25 +13,47 @@ package exercises7;
  */
 public class Person {
 	
+	/**
+	 * instance variables
+	 */
 	private int height, age;
 	
-	public Person(int height) {
+	/**
+	 * constructor
+	 * @param height
+	 */
+	public Person(int height) { 
 		this.height = height;
 		this.age    = 0;
 	}
 	
+	/**
+	 * Mutator
+	 * @param height
+	 */
 	public void setHeight(int height) {
 		this.height = height;
 	}
 	
+	/**
+	 * Other (increases the age of the person by 1)
+	 */
 	public void happyBirthday() {
 		this.age = this.age + 1;
 	}
 	
+	/**
+	 * Accessor
+	 * @return
+	 */
 	public int getHeight() {
 		return this.height;
 	}
 	
+	/**
+	 * Accessor
+	 * @return
+	 */
 	public int getAge() {
 		return this.age;
 	}
