ce9ecedc572f5a3d710bc0b28f61f9361700599c
Bridg <Bridg@Kingdom-69.home>
2017-11-02 00:18:30 -0500

    exercise

---
+++ exercises/exercises8/ThinkingAboutLists.java
@@ -0,0 +1,17 @@
+package exercises8;
+
+public class ThinkingAboutLists {
+
+	// List<Color> could be a list of colors to mix and get a new color
+	//	in this case, order is important, and duplicate could happen
+	
+	// List<Account> is a list of accounts that people could apply for
+	//	in a bank or Frequent Flyer Program Accounts for airlines
+	//	the order might indicate total points or requirement levels
+
+	// List<Integer> or List<String>
+	
+	// if we want to record the answers to a set of True or False questions
+	
+	// if numbers, take the sum or average. compare two lists
+}
