a96213103d7494db449279ae1d05d0d31ba48f38
Bridg <Bridg@Kingdom-69.dhcp.wustl.edu>
2017-11-13 19:15:59 -0600

    lab 8 term

--- labs/lab8/Term.java
+++ labs/lab8/Term.java
@@ -15,10 +15,67 @@ package lab8;
  *    its coefficient is 131.0
  *    its degree      is 0
  * 
- * @author yournamehere
+ * @author bridget
  *
  */
 
 public class Term {
+	
+	private final double coef;
+	private final int degree;
+	
+	public Term(double coef, int degree) {
+		super();
+		this.coef = coef;
+		this.degree = degree;
+	}
+
+	public double getCoefficient() {
+		return coef;
+	}
+
+	public int getDegree() {
+		return degree;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(coef);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + degree;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Term other = (Term) obj;
+		if (Double.doubleToLongBits(coef) != Double.doubleToLongBits(other.coef))
+			return false;
+		if (degree != other.degree)
+			return false;
+		return true;
+	}
+
+	public double evaluateTermAtX(double x) {
+		double result = 0;
+		result = this.coef*Math.pow(x, degree);
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return "Term [coef=" + coef + ", x, ^, degree=" + degree + "]";
+	}
+	
+		
 	
 }
